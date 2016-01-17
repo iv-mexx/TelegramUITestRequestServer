fb0616b672f33caccc7f86d10059c3c4d3324c46
Markus Chmelar <mexx@devsub.net>
2016-01-17 22:44:45 +0100

    "Token" route working

--- app.js
+++ app.js
@@ -3,24 +3,14 @@ var app = express()
 
 // Telegram
 const spawn = require('child_process').spawn;
-const ls = spawn('bash', ['get_token.sh']);
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
 
 // Routes
-app.get('/token', function (req, res) {
-  process.stdout.write("Request Token\n");
-  res.json({"token": "b"});
+app.get('/confirmationCode', function (req, res) {
+  process.stdout.write("Request Confirmation Code\n");
+  const ls = spawn('bash', ['get_confirmation_code.sh']);
+  ls.stdout.on('data', (data) => {
+    res.json({"code": `${data}`.trim()});
+  });
 })
- 
+
 app.listen(3000)
