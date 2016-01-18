25e766f4e330d2b93bc2d5c2df17d4b9090a0f12
Markus Chmelar <mexx@devsub.net>
2016-01-18 14:08:24 +0100

    Implement routes for getting last message and sending message

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+app.use(bodyParser.json())
 
-// Telegram
+// Spawn Bash Scripts
 const spawn = require('child_process').spawn;
 
 // Routes
@@ -11,6 +14,21 @@ app.get('/confirmationCode', function (req, res) {
   ls.stdout.on('data', (data) => {
     res.json({"code": `${data}`.trim()});
   });
-})
+});
+
+app.get('/lastMessage', function (req, res) {
+  process.stdout.write("Request Last Message\n");
+  const ls = spawn('bash', ['get_last_message.sh']);
+  ls.stdout.on('data', (data) => {
+    res.json({"message": `${data}`.trim().replace(/\033\[[0-9;]*m/,"")});
+  });
+});
+
+app.post('/message', function (req, res) {
+  const msg = req.body.message;
+  process.stdout.write("Post message '" + msg + "'\n");
+  const ls = spawn('bash', ['send_message.sh', msg]);
+    res.status(201).json({"message": msg})
+});
 
 app.listen(3000)
